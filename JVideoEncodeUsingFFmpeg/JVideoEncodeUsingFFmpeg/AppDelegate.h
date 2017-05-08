c96e8c04f9d4654893353470dc60fc5c86587de0
Jacky <1142899950@qq.com>
2017-05-08 15:15:10 +0800

    初始化项目

---
+++ JVideoEncodeUsingFFmpeg/JVideoEncodeUsingFFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JVideoEncodeUsingFFmpeg
+//
+//  Created by Jacky on 2017/5/8.
+//  Copyright © 2017年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
